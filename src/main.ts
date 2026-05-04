e9446fcd2c71dce7f73dedf37b4e2c288006e6c0
Mojtaba Fazelinia <mfazelinia@gmail.com>
2026-05-04 16:38:40 +0330

    Setup vite

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+const canvas = document.querySelector("canvas")!;
+const ctx = canvas.getContext("2d")!;
+
+let x = 100;
+
+function update() {
+  x += 1;
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillRect(x, 100, 50, 50);
+}
+
+function gameLoop() {
+  update();
+  draw();
+  requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
