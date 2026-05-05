2a2a16c2b9de7281152642c401ec3dfa3ece555b
Mojtaba Fazelinia <mfazelinia@gmail.com>
2026-05-05 19:59:07 +0330

    Add unit icon

--- src/core/rendering/units-layer.ts
+++ src/core/rendering/units-layer.ts
@@ -11,28 +11,24 @@ export class UnitsLayer extends RenderingLayer {
   draw(unit: Unit) {
     const ctx = this.game.ctx;
     const tileSize = this.game.renderSystem.tileSize;
-    // axial → pixel
-    const x = unit.x; //tileSize * (Math.sqrt(3) * unit.q + (Math.sqrt(3) / 2) * unit.r) + this.game.renderSystem.offsetX;
-    const y = unit.y; //tileSize * (1.5 * unit.r) + this.game.renderSystem.offsetY;
+    const x = unit.x;
+    const y = unit.y;
+
     let unitSize = tileSize * 0.55;
     if (unit.isSelected) unitSize *= 1.2;
 
-    // رنگ از تیم
     ctx.fillStyle = unit.team.color;
     ctx.strokeStyle = '#fff';
-    ctx.lineWidth = unit.isSelected ? 2 : 1;
+    ctx.lineWidth = unit.isSelected ? 3 : 2;
 
-    // دایره بازیکن
     ctx.beginPath();
     ctx.arc(x, y, unitSize, 0, Math.PI * 2);
     ctx.fill();
     ctx.stroke();
 
-    // متن (مثل شماره بازیکن)
-    // ctx.fillStyle = this.team.textColor;
-    // ctx.font = `${size * 0.5}px sans-serif`;
-    // ctx.textAlign = 'center';
-    // ctx.textBaseline = 'middle';
-    // ctx.fillText(this.id, x, y);
+    const img = new Image();
+    img.src = 'src/assets/swordsman.svg';
+    unitSize *= 1.2;
+    ctx.drawImage(img, x - unitSize / 2, y - unitSize / 2, unitSize, unitSize);
   }
 }
