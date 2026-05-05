65579960acab2484d59f54a5e9f76ccd0fb36c95
Mojtaba Fazelinia <mfazelinia@gmail.com>
2026-05-05 19:59:17 +0330

    Fix map

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,12 @@ const canvas = document.getElementById('gameCanvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
 
 const map = new GameMap(mapData);
-const teams = [new Team('Blue', 'blue', 'white'), new Team('Red', 'red', 'white')];
+const teams = [new Team('Blue', '#2F65AC', 'white'), new Team('Red', '#AC392F', 'white')];
 const units = [
-  new Unit(teams[0], { q: -2, r: 0 }),
-  new Unit(teams[0], { q: -1, r: 1 }),
-  new Unit(teams[1], { q: 2, r: 0 }),
-  new Unit(teams[1], { q: 2, r: -1 }),
+  new Unit(teams[0], { q: -2, r: -1 }),
+  new Unit(teams[0], { q: -3, r: 1 }),
+  new Unit(teams[1], { q: 2, r: 1 }),
+  new Unit(teams[1], { q: 3, r: -1 }),
 ];
 
 const game = new Game(ctx, map, teams, units);
--- src/maps/sahra.ts
+++ src/maps/sahra.ts
@@ -1,13 +1,19 @@
 import type { MapConfig } from '../core/map/map-types';
+import type { Coordinate } from '../types';
+
+const createLake = ({ q, r }: Coordinate): MapConfig['terrains'] => [
+  { q: q, r: r - 1, type: 'grass' },
+  { q: q + 1, r: r - 1, type: 'grass' },
+  { q: q + 1, r: r, type: 'grass' },
+  { q: q - 1, r: r, type: 'grass' },
+  { q: q, r: r, type: 'water' },
+  { q: q - 1, r: r + 1, type: 'grass' },
+  { q: q, r: r + 1, type: 'mountain' },
+];
 
 export const sahra: MapConfig = {
   name: 'صحرا',
-  size: [13, 9],
+  size: [21, 13],
   type: 'plain',
-  terrains: [
-    { q: 0, r: 0, type: 'mountain' },
-    { q: 1, r: 1, type: 'grass' },
-    { q: 1, r: 2, type: 'grass' },
-    { q: 2, r: 2, type: 'water' },
-  ],
+  terrains: [{ q: 0, r: 0, type: 'mountain' }, ...createLake({ q: 3, r: 3 }), ...createLake({ q: -3, r: -3 })],
 };
